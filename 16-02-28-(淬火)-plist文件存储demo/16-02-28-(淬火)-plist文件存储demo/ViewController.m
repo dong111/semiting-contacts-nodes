5b03f7f4a7c07cddb0c61e8e55c3476a96226b88
陈栋 <chendong@yitaosoft.com>
2016-02-28 22:06:48 +0800

    string array dictionary 三个对象自带写入文件和读取文件方法

--- 16-02-28-(淬火)-plist文件存储demo/16-02-28-(淬火)-plist文件存储demo/ViewController.m
+++ 16-02-28-(淬火)-plist文件存储demo/16-02-28-(淬火)-plist文件存储demo/ViewController.m
@@ -9,42 +9,82 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (nonatomic,copy) NSString *plistPath;
 
 @end
 
 @implementation ViewController
+//初始化plist沙盒目录
+- (NSString *)plistPath
+{
+    if (_plistPath==nil) {
+        NSString *doc = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+//        NSLog(@"documentPath : %@",doc);
+        
+        //document 目录中添加文件
+         _plistPath = [doc stringByAppendingPathComponent:@"data.plist"];
+    }
+    return _plistPath;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     //获取应用沙盒目录
     //TheNSSearchPathDirectoryse constants specify the location of a variety of directories by the
     
-    NSString *doc = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
-    NSLog(@"documentPath : %@",doc);
-
-    //document 目录中添加文件
-    NSString *listPath = [doc stringByAppendingPathComponent:@"data.plist"];
-    NSLog(@"listPath : %@",listPath);
-
-    
-    //读取数据
-    NSArray *array = [NSArray arrayWithContentsOfFile:listPath];    NSLog(@"%@",array);
 }
-//plist文件中数据写入
+
+
+
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
-    NSArray *array = @[@"test1",@"test2"];
-    
-    NSString *doc = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
-   
-    
-    //document 目录中添加文件
-    NSString *listPath = [doc stringByAppendingPathComponent:@"data.plist"];
-    
-    
-    //写入数据
-    [array writeToFile:listPath atomically:YES];
-
+    [self testString2Plist];
 }
+
+
+/**
+ *  测试Dictionary存储在plist文件中
+ *
+ *
+ */
+
+- (void) testString2Plist
+{
+    NSString *orginString = @"orginString";
+    [orginString writeToFile:self.plistPath atomically:YES encoding:NSUTF8StringEncoding error:nil];
+    
+    NSString *getString = [NSString stringWithContentsOfFile:self.plistPath encoding:NSUTF8StringEncoding error:nil];
+    
+    NSLog(@"%@",getString);
+}
+
+
+/**
+ *  测试Dictionary存储在plist文件中
+ */
+- (void) testDictionary
+{
+    NSDictionary *dict = @{@"name":@"xiaoming",@"age":@15};
+    [dict writeToFile:self.plistPath atomically:YES];
+    
+    NSDictionary *readPlistDict = [NSDictionary dictionaryWithContentsOfFile:self.plistPath];
+    
+    NSLog(@"%@",readPlistDict);
+}
+
+/**
+ *  测试NSarray存储在plist文件中
+ */
+- (void) testArray
+{
+    NSArray *array = @[@"array1",@"array2"];
+    //写入一个文件
+    [array writeToFile:self.plistPath atomically:YES];
+    
+    //读取数据
+    NSArray *destArray =  [NSArray arrayWithContentsOfFile:self.plistPath];
+    NSLog(@"%@",destArray);
+}
+
 
 @end
