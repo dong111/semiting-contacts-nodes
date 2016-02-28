ef096da9f10f3e3bc3c9ae40dc63c627156b8bd2
陈栋 <chendong@yitaosoft.com>
2016-02-28 23:04:12 +0800

    NSCoding（归档）保存数据Demo
    NSCoding（归档）保存数据 新创建项目

---
+++ 16-02-28-(淬火)-NSCoding/16-02-28-(淬火)-NSCoding/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  16-02-28-(淬火)-NSCoding
+//
+//  Created by 陈栋 on 16/2/28.
+//  Copyright © 2016年 man. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ 16-02-28-(淬火)-NSCoding/16-02-28-(淬火)-NSCoding/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  16-02-28-(淬火)-NSCoding
+//
+//  Created by 陈栋 on 16/2/28.
+//  Copyright © 2016年 man. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
