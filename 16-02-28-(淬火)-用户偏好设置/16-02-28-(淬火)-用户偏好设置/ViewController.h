05afb9d5f1fa1d8b782139b7c37317d733112c7d
陈栋 <chendong@yitaosoft.com>
2016-02-28 22:30:11 +0800

    添加用户偏好设置项目
    添加用户偏好设置项目

---
+++ 16-02-28-(淬火)-用户偏好设置/16-02-28-(淬火)-用户偏好设置/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  16-02-28-(淬火)-用户偏好设置
+//
+//  Created by 陈栋 on 16/2/28.
+//  Copyright © 2016年 man. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 16-02-28-(淬火)-用户偏好设置/16-02-28-(淬火)-用户偏好设置Tests/_6_02_28____________Tests.m
@@ -0,0 +1,39 @@
+//
+//  _6_02_28____________Tests.m
+//  16-02-28-(淬火)-用户偏好设置Tests
+//
+//  Created by 陈栋 on 16/2/28.
+//  Copyright © 2016年 man. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _6_02_28____________Tests : XCTestCase
+
+@end
+
+@implementation _6_02_28____________Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
