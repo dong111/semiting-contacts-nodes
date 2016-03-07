a62f2f6a8d1975f1acf4454d913ec531288e191b
陈栋 <chendong@yitaosoft.com>
2016-03-07 23:24:27 +0800

    (非代码)Quartz 画图 项目创建
    (非代码)Quartz 画图 项目创建

---
+++ 16-03-07-(熔炼)-Quartz-画图/16-03-07-(熔炼)-Quartz-画图Tests/_6_03_07______Quartz___Tests.m
@@ -0,0 +1,39 @@
+//
+//  _6_03_07______Quartz___Tests.m
+//  16-03-07-(熔炼)-Quartz-画图Tests
+//
+//  Created by 陈栋 on 16/3/7.
+//  Copyright © 2016年 man. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _6_03_07______Quartz___Tests : XCTestCase
+
+@end
+
+@implementation _6_03_07______Quartz___Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
