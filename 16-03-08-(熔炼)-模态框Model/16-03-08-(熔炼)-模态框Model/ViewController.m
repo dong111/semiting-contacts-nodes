ae451e9f178308130d23cc736d895e6790936eca
陈栋 <chendong@yitaosoft.com>
2016-03-08 12:33:57 +0800

    (非代码)模态框项目搭建
    (非代码)模态框项目搭建

---
+++ 16-03-08-(熔炼)-模态框Model/16-03-08-(熔炼)-模态框Model/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  16-03-08-(熔炼)-模态框Model
+//
+//  Created by 陈栋 on 16/3/8.
+//  Copyright © 2016年 man. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
