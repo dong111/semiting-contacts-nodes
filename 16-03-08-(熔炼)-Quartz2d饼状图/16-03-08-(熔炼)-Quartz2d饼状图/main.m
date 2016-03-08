71c336e79b862e404d8d03a44574815568b79011
陈栋 <chendong@yitaosoft.com>
2016-03-08 12:57:57 +0800

    (非代码)Quartz2D画饼状图
    (非代码)Quartz2D画饼状图

---
+++ 16-03-08-(熔炼)-Quartz2d饼状图/16-03-08-(熔炼)-Quartz2d饼状图/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  16-03-08-(熔炼)-Quartz2d饼状图
+//
+//  Created by 陈栋 on 16/3/8.
+//  Copyright © 2016年 man. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
