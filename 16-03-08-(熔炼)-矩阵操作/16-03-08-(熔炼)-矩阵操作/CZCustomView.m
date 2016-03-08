7cd68b436eb92369d6fd934433acbef03f0735f7
陈栋 <chendong@yitaosoft.com>
2016-03-08 17:29:28 +0800

    图形 平移 旋转 缩放
    平移 旋转 缩放

--- 16-03-08-(熔炼)-矩阵操作/16-03-08-(熔炼)-矩阵操作/CZCustomView.m
+++ 16-03-08-(熔炼)-矩阵操作/16-03-08-(熔炼)-矩阵操作/CZCustomView.m
@@ -22,10 +22,28 @@
     //获取CGContextRef 上下文
      CGContextRef ctx = UIGraphicsGetCurrentContext();
     
+    
+#pragma -mark 平移 旋转 缩放要在绘制之前
+    //平移
+//    CGContextTranslateCTM(ctx, 100,50);
+    
+    //缩放
+    //CGContextScaleCTM(ctx, 1.5, 1.5);
+    
+    //旋转
+    // 负数 逆时针/ 正数 顺时针
+    // 围绕左上角(0,0) 旋转
+    CGContextRotateCTM(ctx, - M_PI * 0.25);
+    
+    
     //通过3个点画三角形
     CGPoint points[3] = {{50,20},{100,80},{10,80}};
     CGContextAddLines(ctx, points, 3);
     CGContextClosePath(ctx);//合并路径
+    
+    //点划线
+    CGPoint points2[2] = {{50,20},{60,80}};
+    CGContextAddLines(ctx, points2, 2);
     
     
     CGContextStrokePath(ctx);
