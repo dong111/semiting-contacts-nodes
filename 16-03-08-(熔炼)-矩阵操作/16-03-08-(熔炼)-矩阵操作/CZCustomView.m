587152e93db2876a3afabdd75b473f792e68e1ed
陈栋 <chendong@yitaosoft.com>
2016-03-08 17:14:10 +0800

    获取CGContextRef 上下文

---
+++ 16-03-08-(熔炼)-矩阵操作/16-03-08-(熔炼)-矩阵操作/CZCustomView.m
@@ -0,0 +1,27 @@
+//
+//  CZCustomView.m
+//  16-03-08-(熔炼)-矩阵操作
+//
+//  Created by 陈栋 on 16/3/8.
+//  Copyright © 2016年 man. All rights reserved.
+//
+
+#import "CZCustomView.h"
+
+@implementation CZCustomView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+- (void)drawRect:(CGRect)rect {
+    //获取CGContextRef 上下文
+     CGContextRef ctx = UIGraphicsGetCurrentContext();
+    
+}
+
+@end
