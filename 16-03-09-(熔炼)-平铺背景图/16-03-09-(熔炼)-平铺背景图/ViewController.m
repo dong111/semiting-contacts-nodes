a63ab61d1ff39eeb4ca752ca25ad8f0776d0a560
陈栋 <chendong@yitaosoft.com>
2016-03-09 11:44:11 +0800

    (非代码)平铺背景图  demo
    (非代码)平铺背景图  demo

---
+++ 16-03-09-(熔炼)-平铺背景图/16-03-09-(熔炼)-平铺背景图/ViewController.m
@@ -0,0 +1,40 @@
+//
+//  ViewController.m
+//  16-03-09-(熔炼)-平铺背景图
+//
+//  Created by 陈栋 on 16/3/9.
+//  Copyright © 2016年 man. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ 16-03-09-(熔炼)-平铺背景图/16-03-09-(熔炼)-平铺背景图/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  16-03-09-(熔炼)-平铺背景图
+//
+//  Created by 陈栋 on 16/3/9.
+//  Copyright © 2016年 man. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
