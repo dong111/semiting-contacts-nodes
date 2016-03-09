e9e9939876135fe21426e77c9b38c0fb8eade229
陈栋 <chendong@yitaosoft.com>
2016-03-09 10:02:06 +0800

    (非代码)提交水印图项目
    (非代码)提交水印图项目

---
+++ 16-03-09-(熔炼)-水印图片/16-03-09-(熔炼)-水印图片/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  16-03-09-(熔炼)-水印图片
+//
+//  Created by 陈栋 on 16/3/9.
+//  Copyright © 2016年 man. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 16-03-09-(熔炼)-水印图片/16-03-09-(熔炼)-水印图片Tests/_6_03_09__________Tests.m
@@ -0,0 +1,39 @@
+//
+//  _6_03_09__________Tests.m
+//  16-03-09-(熔炼)-水印图片Tests
+//
+//  Created by 陈栋 on 16/3/9.
+//  Copyright © 2016年 man. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _6_03_09__________Tests : XCTestCase
+
+@end
+
+@implementation _6_03_09__________Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
