7897f30c1d8125bcd70d31072862293df6ba8aae
陈栋 <chendong@yitaosoft.com>
2016-03-09 10:29:31 +0800

    图片保存指定路径逻辑
     //将水印图片显示到ImageView
    //将水印图片保存到本地路径

--- 16-03-09-(熔炼)-水印图片/16-03-09-(熔炼)-水印图片/ViewController.m
+++ 16-03-09-(熔炼)-水印图片/16-03-09-(熔炼)-水印图片/ViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "UIImage+CD.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
@@ -16,8 +18,32 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+   //将水印图片显示到ImageView
+    self.imageView.image = [UIImage waterImageWithBgImage:@"" waterName:@""];
+    
+    //将水印图片保存到本地路径
+    [self saveImage:self.imageView.image ToPath:@""];
+    
 }
+
+
+/**
+ *  图片保存到指定路径
+ *
+ *  @param image 要保存图片
+ *  @param path  保存路径
+ */
+- (void) saveImage:(UIImage *) image ToPath:(NSString *) path
+{
+    //将图片装换成二进制文件才能保存
+    NSData *imageData = UIImagePNGRepresentation(image);
+    
+    //二进制文件保存da
+    //    If YES, the data is written to a backup file, and then—assuming no errors occur—the backup file is renamed to the name specified by path; otherwise, the data is written directly to path.
+    [imageData writeToFile:path atomically:YES];
+    
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
